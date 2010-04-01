51cf94ad39be40402c1b7865075cf8005319f95c
ybian <ybian>
2010-04-01 15:00:57 +0000

    Ajout de statut OK (sans coordonnées) Modification de friendship OK
    A faire : gérer l'ajout de message avec coordonnées, la modification et la suppression

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
  class User < ActiveRecord::Base
-   has_many :friendships
-   has_many :friends, :through => :friendships
+	has_many :friendships
+	has_many :friends, :through => :friendships
+	has_many :statuses
+	
     acts_as_authentic do |c|
-      c.validate_email_field = true # for available options see documentation in: Authlogic::ActsAsAuthentic
-    end # block optional
-  end
+    	c.validate_email_field = true # for available options see documentation in: Authlogic::ActsAsAuthentic
+	end # block optional
+end
--- config/routes.rb
+++ config/routes.rb
@@ -43,7 +43,7 @@ ActionController::Routing::Routes.draw do |map|
   
   map.root :controller => :user_sessions, :action => :new
   
-  map.resources :users, :has_many => :friendships, :collection => { :search => :get }
+  map.resources :users, :collection => { :search => :get }, :has_many => [:friendships, :statuses]
   #map.resources :friendships, :collection => { :search => :get }, :path_prefix => 'users/:user_id'
   map.resource :session, :controller => :user_sessions, :collection => { :destroy => :get }
   
